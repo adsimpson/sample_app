bc7777d35bf47e89022b23ed6ac962740d7fb4a1
Adam Simpson <ad_simpson@hotmail.com>
2013-09-16 23:00:38 +0000

    Finish user edit, update, index and destroy actions

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -4,6 +4,53 @@ describe "User pages" do
 
   subject { page }
 
+  describe "index" do
+    let(:user) { FactoryGirl.create(:user) }
+    before(:each) do
+      sign_in user
+      visit users_path
+    end
+
+    describe "page" do
+      it { should have_content('All users') }
+      it { should have_title('All users') }
+    end
+    
+    describe "pagination" do
+      before(:all) { 30.times { FactoryGirl.create(:user) } }
+      after(:all)  { User.delete_all }
+
+      it { should have_selector('div.pagination') }
+
+      it "should list each user" do
+        User.paginate(page: 1).each do |user|
+          expect(page).to have_selector('li', text: user.name)
+        end
+      end
+    end
+    
+    describe "delete links" do
+
+      it { should_not have_link('delete') }
+
+      describe "as an admin user" do
+        let(:admin) { FactoryGirl.create(:admin) }
+        before do
+          sign_in admin
+          visit users_path
+        end
+
+        it { should have_link('delete', href: user_path(User.first)) }
+        it "should be able to delete another user" do
+          expect do
+            click_link('delete', match: :first)
+          end.to change(User, :count).by(-1)
+        end
+        it { should_not have_link('delete', href: user_path(admin)) }
+      end
+    end
+  end
+  
   describe "profile page" do
     let(:user) { FactoryGirl.create(:user) }
     before { visit user_path(user) }
@@ -12,47 +59,88 @@ describe "User pages" do
     it { should have_title(user.name) }
   end
   
-  describe "signup page" do
-    before { visit signup_path }
-
-    it { should have_content('Sign up') }
-    it { should have_title(full_title('Sign up')) }
-  end
   
   describe "signup" do
     before { visit signup_path }
-    let(:submit) { "Create my account" }
+
+    describe "page" do
+      it { should have_content('Sign up') }
+      it { should have_title('Sign up') }
+    end
 
     describe "with invalid information" do
+      
       it "should not create a user" do
-        expect { click_button submit }.not_to change(User, :count)
+        expect { invalid_sign_up }.not_to change(User, :count)
       end
       
       describe "after submission" do
-        before { click_button submit }
-
-        it { should have_title(full_title('Sign up')) }
+        before { invalid_sign_up }
+        it { should have_title('Sign up') }
         it { should have_content('error') }
       end
       
     end
 
     describe "with valid information" do
-      before { valid_signup }
+      let(:user) { FactoryGirl.build(:user) }
 
       it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
+        expect { sign_up user }.to change(User, :count).by(1)
       end
       
       describe "after saving the user" do
-        before { click_button submit }
-        let(:user) { User.find_by(email: 'user@example.com') }
-
+        before { sign_up user }
+        let(:found_user) { User.find_by(email: user.email) }
         it { should have_link('Sign out') }
-        it { should have_title(user.name) }
+        it { should have_title(found_user.name) }
         it { should have_success_message('Welcome') }
       end
       
+    end
+    
+  end
+ 
+  
+  describe "edit" do
+    let(:user) { FactoryGirl.create(:user) }
+    before do
+      sign_in user 
+      visit edit_user_path(user)
+    end
+
+    describe "page" do
+      it { should have_content("Update your profile") }
+      it { should have_title("Edit user") }
+      it { should have_link('change', href: 'http://gravatar.com/emails') }
+    end
+
+    describe "with invalid information" do
+      before { click_button "Save changes" }
+      it { should have_content('error') }
+    end
+    
+    describe "with valid information" do
+      let(:new) { {name: 'New Name', email: 'new@example.com'} }
+      before { edit_profile(new,user) }
+
+      it { should have_title(new[:name]) }
+      it { should have_success_message('Profile updated') }
+      # it { should have_link('Sign out') }
+      specify { expect(user.reload.name).to  eq new[:name] }
+      specify { expect(user.reload.email).to eq new[:email] }
+    end
+    
+    describe "forbidden attributes" do
+      let(:params) do
+        { user: { admin: true, password: user.password,
+                  password_confirmation: user.password } }
+      end
+      before do
+        sign_in user, no_capybara: true
+        patch user_path(user), params
+      end
+      specify { expect(user.reload).not_to be_admin }
     end
     
   end
--- spec/support/utilities.rb
+++ spec/support/utilities.rb
@@ -1,20 +1,42 @@
 include ApplicationHelper
 
-def valid_signin(user)
-  fill_in "Email",    with: user.email
-  fill_in "Password", with: user.password
+def sign_in(user, options={})
+  if options[:no_capybara]
+    # Sign in when not using Capybara.
+    remember_token = User.new_remember_token
+    cookies[:remember_token] = remember_token
+    user.update_attribute(:remember_token, User.encrypt(remember_token))
+  else
+    visit signin_path    
+    fill_in "Email",           with: user.email
+    fill_in "Password",        with: user.password
+    click_button "Sign in"
+  end
+end
+
+def invalid_sign_in
+  visit signin_path    
   click_button "Sign in"
 end
 
-def invalid_signin
-  click_button "Sign in"
+def sign_up(user)
+  fill_in "Name",              with: user.name
+  fill_in "Email",             with: user.email
+  fill_in "Password",          with: user.password
+  fill_in "Confirmation",      with: user.password
+  click_button "Create my account"
 end
 
-def valid_signup
-  fill_in "Name",    with: "Example User"
-  fill_in "Email",    with: "user@example.com"
-  fill_in "Password", with: "foobar"
-  fill_in "Confirmation", with: "foobar"
+def invalid_sign_up
+  click_button "Create my account"
+end
+
+def edit_profile(new, user)
+  fill_in "Name",             with: new[:name]
+  fill_in "Email",            with: new[:email]
+  fill_in "Password",         with: user.password
+  fill_in "Confirm Password", with: user.password_confirmation
+  click_button "Save changes"
 end
 
 RSpec::Matchers.define :have_error_message do |message|
